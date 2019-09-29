b8860d4ae038b9528c796a918d2f45424814b25e
Peter Kamau <peterndungukamau9@gmail.com>
2019-09-30 00:21:29 +0300

    add toggle on what we do image and paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,30 @@ $(document).ready(function(){
         $("h1").addClass("wel")
     });
 });
+
+$(document).ready(function(){
+    $("h5#well").click(function(){
+        $("h5").addClass("well")
+    });
+});
+
+$(document).ready(function(){
+    $("img#repo1").click(function(){
+        $("img#repo1").toggle();
+        $("p#repos1").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("img#repo2").click(function(){
+        $("img#repo2").toggle();
+        $("p#repos2").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("img#repo3").click(function(){
+        $("img#repo3").toggle();
+        $("p#repos3").toggle();
+    });
+});
